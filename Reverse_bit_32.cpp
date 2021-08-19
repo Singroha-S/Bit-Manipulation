748b2bbc843e8058e62a3f538ff28869e953632d
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-19 21:51:13 +0530

    Update Reverse_bit_32.cpp

--- Reverse_bit_32.cpp
+++ Reverse_bit_32.cpp
@@ -24,3 +24,19 @@ int main()
 
     return 0;
 }
+
+   for(int  i = 31; i >= 0; i--){
+         rev |= ( (n&1) << i);
+         n = n >> 1;
+         
+     }
+ 	return rev;
+ 	
+
+
+   for(int i = 0; i<32; i++){
+       rev = rev << 1;
+       rev |= (n & 1);
+       n = n>>1;
+   }
+   return rev;
