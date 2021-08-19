d449b557879602a146e0fb46e58005915d64f750
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2021-08-19 14:12:47 +0530

    Update Count_ones.cpp

--- Count_ones.cpp
+++ Count_ones.cpp
@@ -17,3 +17,29 @@ int main()
     cout<<count_ones(n);
     return 0;
 }
+
+#include<bits/stdc++.h>
+using namespace std;
+int count_set(int n)
+{
+    int cnt=0;
+    while(n>0)
+    {
+        int a = n&1;
+        if(a==1)
+        {
+           cnt++;
+           n>>=1;
+        }
+        else
+           n>>=1;
+    }
+    return cnt;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<count_set(n);
+    return 0;
+}
